9a78c6e6c70b851ccc718e4ccee49c8165d36f1d
jimin Kim <programmer0415@gmail.com>
2022-12-12 16:40:21 +0900

    Feat: StompAllocator OverFlow 정책 추가

--- ServerCore/Allocator.cpp
+++ ServerCore/Allocator.cpp
@@ -13,12 +13,19 @@ void BaseAllocator::Release(void* ptr)
 
 void* StompAllocator::Alloc(int32 size)
 {
-	//4 -> 4096	
+	// [        4kb         [allocate]]
 	const int64 pageCount = (size + PAGE_SIZE - 1) / PAGE_SIZE;
-	return ::VirtualAlloc(NULL, pageCount * PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	const int64 dataOffset = pageCount * PAGE_SIZE - size;
+	void* baseAddress =::VirtualAlloc(NULL, pageCount * PAGE_SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+
+	//								1byte 로 치환 후 offset 이동
+	return static_cast<void*>(static_cast<int8*>(baseAddress) + dataOffset);
 }
 
 void StompAllocator::Release(void* ptr)
 {
-	::VirtualFree(ptr, 0, MEM_RELEASE);
+	//[				[allocator]] <- 할당 초기 메모리로 떙긴 후 메모리 할당 해제
+	const int64 address = reinterpret_cast<int64>(ptr);
+	const int64 baseAddress = address - (address % PAGE_SIZE); //[ /////// []] 해당 크기 잡음
+	::VirtualFree(reinterpret_cast<void*>(baseAddress), 0, MEM_RELEASE);
 }
