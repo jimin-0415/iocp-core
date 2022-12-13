63c4365d9963516b8db8f7ff62b801222f38dc5a
jimin Kim <programmer0415@gmail.com>
2022-12-13 17:13:22 +0900

    Docs: Study Base TCP/IP

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,7 +1,60 @@
-﻿#include "pch.h"
+#include "pch.h"
+
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-    HelloWorld();
+    //Winsock 라이브러리 초기화 ws2_32 초기화
+    WSAData wsaData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        return 0;
+
+    //af : address Familiy 주소 체계 
+    //type : Tcp, Udp
+    //protocol : 0
+    // return : descriptor <- 정수 받은 각 socket 의 식별 번호
+    SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket == INVALID_SOCKET) {
+        int errCode = ::WSAGetLastError();
+        cout << "Socket ErrorCode :" << errCode << endl;
+        return 0;
+    }
+
+    SOCKADDR_IN serverAddr; //ip4 일경우 이 구조체 사용
+    ::memset(&serverAddr, 0, sizeof(serverAddr)); //메모리 초기화
+    serverAddr.sin_family = AF_INET;
+    //serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1") //너무 옛날 버전
+    ::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+    serverAddr.sin_port = ::htons(7777);    //http : 80 같은 번호는 예약이 되어 있다.
+
+    //host to network short
+    //Little-Endian vs Big-Endian
+    //ex) 0x12345678    4바이트 정수 저장
+    // low [0x78][0x56][0x34][0x12] high <   little , cpu에 따라서 다르다, 요즘은 모두 little 엔디안 방식 [client] 
+    // <- 로컬 컴퓨터 환경은 little 방식이다
+     // low [0x12][0x34][0x56][0x78] high <   Big -> 받는 쪽은 반대로 받아야 한다. bigEndian 방식으로. [server]
+    
+    if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        int errCode = ::WSAGetLastError();
+        cout << "Connet ErrorCode :" << errCode << endl;
+        return 0;
+    }
+
+    //Connect Success
+    cout << "Connected To Server!" << endl;
+
+    while (true) {
+
+        this_thread::sleep_for(1s);
+    }
+
+    //소켓 리소스 반환
+    ::closesocket(clientSocket);
+
+    //윈속 종료
+    ::WSACleanup();
 }
 
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -15,115 +15,62 @@
 
 #include "TypeCast.h"
 
-enum PLAYER_TYPE {
-	KNIGHT,
-	MONSTER,
-};
-
-class Player 
-{
-public:
-	Player() {
-	}
-	virtual ~Player();
-
-	int _type;
-};
-
-class Knight : public Player
-{
-public:
-	Knight() {
-		_type = KNIGHT;
-	}
-public :
-	int32 _hp = rand() % 100;
-};
-
-class Monster : public Player
-{
-public:
-	Monster() {
-		_type = MONSTER;
-	}
-public:
-	int64 _id = 0;
-};
-
-class A {
-
-};
-
+#include <WinSock2.h>
+#include <mswsock.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
 
 int main()
 {
-	//대부분 프로젝트는 상위 객체로 관리하다가
-	//다시 다운 캐스팅을 해야한다.
-	Player* p1 = new Knight();
-	Player* p2 = new Monster();
+    //Winsock 라이브러리 초기화 ws2_32 초기화
+    WSAData wsaData;
+    if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+        return 0;
 
-	Knight* k1 = static_cast<Knight*>(p2);
-	Knight* k2 = dynamic_cast<Knight*>(p2);  
+    SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+    if (listenSocket == INVALID_SOCKET) {
+        int errCode = ::WSAGetLastError();
+        cout << "Socket ErrorCode :" << errCode << endl;
+        return 0;
+    }
 
-	int32 len1 = Length<TypeList<Player, Monster, Knight>>::value;
+    SOCKADDR_IN serverAddr; //ip4 일경우 이 구조체 사용
+    ::memset(&serverAddr, 0, sizeof(serverAddr)); //메모리 초기화
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);   //모든 주소 연결 
+    serverAddr.sin_port = ::htons(7777);    //http : 80 같은 번호는 예약이 되어 있다.
 
-	using TL = TypeList<Player, Monster, Knight>;
-	TypeAt<TL, 0>::Result typeAt0; //Player
-	TypeAt<TL, 1>::Result typeAt1; //Monster
-	TypeAt<TL, 2>::Result typeAt2; //Knight
+    if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        int errCode = ::WSAGetLastError();
+        cout << "Bind ErrorCode :" << errCode << endl;
+        return 0;
+    }
 
-	int index = IndexOf<TL, Player>::value;
-	int index2 = IndexOf<TL, Monster>::value;
-	int index3 = IndexOf<TL, Knight>::value;
-	int index4 = IndexOf<TL, A>::value;		//-1 못찾음
+    //일단 대기열 10개
+    if (::listen(listenSocket, 10) == SOCKET_ERROR) {
+        int errCode = ::WSAGetLastError();
+        cout << "Listen ErrorCode :" << errCode << endl;
+        return 0;
+    }
 
-	//Static Cast 는 위험다.
-	//Dynamic Cast는 안전하지만, 성능상 좋지 않다.
+    while (true) {
+        SOCKADDR_IN clientAddr;
+        ::memset(&clientAddr, 0, sizeof(clientAddr)); //메모리 초기화
+        int32 addresslen = sizeof(clientAddr);
 
-	//1. 성능 개선 -> enum을 두고 enum check후 static_cast 를 진행하낟.
-	if(k1->_type == KNIGHT)
-		Knight* k1 = static_cast<Knight*>(p2);
+        SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addresslen);
+        if (clientSocket == INVALID_SOCKET) {
+            int errCode = ::WSAGetLastError();
+            cout << "Socket Accept ErrorCode :" << errCode << endl;
+            return 0;
+        }
 
-	//1-1. 문제점. -> 클래스가 많아지면 일일이 코드 만드는게 일이다.
-	//1-2. 체크 누락시 문제가 발생
-	//1-3. 여러작업자중 누군가가 당연히 캐스팅 해도 되겠지 하는 경우도 발생.
+        char ipAddress[16];
+        ::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+        cout << "Client Connected IP = " << ipAddress << endl;
+    }
 
-	//2. 함수를 만들어서 관리한다. -> 이것도 일일이 만들어야 한다.
-
-
-
-	//Object Pool 수동 관리 부분.
-	Knight* k =  ObjectPool<Knight>::Pop();
-	ObjectPool<Knight>::Push(k);
-
-	Knight* knight[100];
-
-	for (int32 i = 0; i < 100; ++i) {
-		knight[i] = ObjectPool<Knight>::Pop();
-	}
-
-	for (int32 i = 0; i < 100; ++i) {
-		ObjectPool<Knight>::Push(knight[i]);
-	}
-	
-	//ObjectPool Knight 생성
-	shared_ptr<Knight> sptr = ObjectPool<Knight>::MakeShared();
-	
-	//Memory Pool Knight 생성
-	shared_ptr<Knight> sptr2 = MakeShared<Knight>();
-
-	for (int32 i = 0; i < 5; ++i) {
-		GThreadManager->Launch([]() {
-			while (true) {
-				Knight* a = xnew<Knight>();
-				cout << a->_hp << endl;
-
-				this_thread::sleep_for(10ms);
-
-				xdelete(a);
-			}
-		});
-	}
+    ::WSACleanup();
 	return 0;
 }
 
