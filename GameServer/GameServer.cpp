a10b307292e89336762c22ac095b16acc4c797f5
jimin Kim <programmer0415@gmail.com>
2022-12-14 12:04:26 +0900

    Docs: Study Select Model

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -25,11 +25,13 @@ void HandleError(const char* cuase) {
     cout << cuase << errCode << endl;
 }
 
-//블로킹을 논 블로킹으로 바꾼다고 해서, 성능 이점이 없다.
-//현재 코드를 보면 계속적으로 Connection, Send, Recv가 오는지 확인하기 위해서 while문으로 루프를 보고있다.
-//불필요하게 체크를 하기 때문에 CPU를 계속 점유한 상태가 된다.
-//1. 소켓은 옵션을 바꾸면 논블로킹으로 바뀌는구나.
-//2. 논블로킹으로 바꾼다고 해서 모든 문제가 해결되는것은 아니다. - CPU 사이클 낭비를 줄여야 한다. 여러 소켓 모델로 해결
+const int32 BUFSIZE = 1000;
+struct Session {
+    SOCKET socket;
+    char recvBuffer[BUFSIZE] = {};
+    int32 recvBytes = 0;
+    int32 sendBytes = 0;
+};
 
 int main()
 {
@@ -37,12 +39,6 @@ int main()
     WSAData wsaData;
     if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         return 0;
-
-    SOCKET serverSocket = ::socket(AF_INET, SOCK_DGRAM, 0);
-    if (serverSocket == INVALID_SOCKET) {
-        HandleError("Socket");
-        return 0;
-    }
 
     SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
     if (listenSocket == INVALID_SOCKET)
@@ -69,58 +65,122 @@ int main()
 
     cout << "Accept" << endl;
 
-    SOCKADDR_IN clientAddr;
-    int32 addrLen = sizeof(clientAddr);
+    //SELECT 모델 = (select 함수가 핵심이 된다)
+    // Socekt 함수 호출이 성공할 시점을 미리 알 수 있다. - blocking, Non-Blocking 모두 적용 가능.
+    //문제 상황 > <- 수신 버퍼에 데이터가 없을때, read한다는 상황.
+    //문제상황 2 > <- 송신버퍼가 꽉 찼을때 write한다거나 하는 상황.
 
-    //Accept
+    //- 블로킹 소켓 : 조건이 만족되지 않아서 블로킹 되는 상황 방지.
+    //- 논블로킹 소켓 : 조건이 만족되지 않아서 불필요하게 반복체크하는 상황 예방.
+
+    //socket set
+    //1. 읽기[] 쓰기[] 예외 [] 관찰 대상을 등록한다. 모두 넣을 필요 없음
+    //원하는 관찰 대상을 넣는다. 
+    //예외 ? OutOfBand - Send() 마지막 인자 MSG_OOB로 특수 데이터를 보냄
+    //받는 쪽에서도 recv OOB 세팅을 해야 읽을 수 있다.
+    //2. select(readSet, writeSet, exceptSet); 을 넣어서 관찰 시작.
+    //3. 적어도 하나의 소켓이 준비되면 리턴 <- 낙오자는 알아서 제거됨.
+    // ex > 1,2,3 소켓을 대상으로 일기를 하는데.. 이것을 socekt set에 등록해서 체크를 한다.
+    // 만약여기서 select를 호출하면 1.2.3을 관찰하다가 만약 2번에 데이터가 와서 수신버퍼가 왔다면 개수를 반환해 준다.
+    // 그러면 읽기에는 2만 남고 나머지 1,3는 제거된다. -> 처리를 끝나면 다시 1,2,3을 읽기 select에 세팅한다.
+
+    //함수 호출 을 체크하고 한다.
+    ///장점 : 구현이 간단하고, 리소스 낭비를 줄일 수 있다.
+    //담점 : 반복적으로 등록에 문제가 생김, FD_SETSIZE <- 64개까지밖에 등록을 못한다. fd_set 하나에...만약 640명이면 640개를 만들면된다.
+
+    //fd_set read;
+    // FD_ZERO : 비운다.
+    //FD_ZERO(set);
+
+    //FD_SET : 소켓 s를 넣는다.
+    //FD_SET(s, &set);
+    // 
+    //FD_CLR : 소켓 s 를 제거
+    //FD_CLR(s, &set);
+    
+    //FD_ISSET : 소켓 s가 set에 들어가 있으면 0이 아닌값을 리턴한다.
+
+    vector<Session> sessions;
+    sessions.reserve(100);
+
+    fd_set reads;
+    fd_set writes;
+
     while (true) {
-        SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-        if (clientSocket == INVALID_SOCKET)
-        {
-            if (::WSAGetLastError() == WSAEWOULDBLOCK)
-                continue;
+        //set은 매 루프마다 초기화 시켜준다.
+        FD_ZERO(&reads);
+        FD_ZERO(&writes);
+    
+        //FD_SET 은 계속적으로 등록함, 만약에 누군가 호출되면 낙오자는 자연스럽게 제거되기 때문에, 다시 등록 필요.
+        //socket 등록
+        FD_SET(listenSocket, &reads);
 
-            //Error
-            break;
+        //socket 등록
+        for (Session& s : sessions) {
+            //현재는 echo 서버로 작동한다. 만약 recv를 받았으면 보내기를 해야 한다. 그러면 Sendbyte의 크기가 커지기 때문에
+            //아래와 같은 if문이 생성된다.
+            if (s.recvBytes <= s.sendBytes) 
+                FD_SET(s.socket, &reads);
+            else
+                FD_SET(s.socket, &writes);
         }
 
-        cout << "Client Connected !" << endl;
+        //마지막에 timout인자 설정 가능
+        //timeval timeOut;
+        //timeOut.tv_sec; //sec
+      
+        int32 retVal = ::select(0, &reads, &writes, nullptr, nullptr);
+        if (retVal == SOCKET_ERROR)
+            break;
 
-        //Recv
-        while (true) {
-            char recvBuffer[1000];
-            int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-            if (recvLen == SOCKET_ERROR) {
-                if (::WSAGetLastError() == WSAEWOULDBLOCK)
+        //Listen소켓 체크 -> listen 소켓이 준비된 상태이다
+        if (FD_ISSET(listenSocket, &reads)) {
+            SOCKADDR_IN  clientAddr;
+            int32 addrLen = sizeof(clientAddr);
+            SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+            if (clientSocket != INVALID_SOCKET) {
+                cout << "Client Connected " << endl;
+                sessions.push_back( Session{ clientSocket } );
+            }
+        }
+
+        //나머지 소켓
+        for (Session& s : sessions) {
+            //Read Check
+            if (FD_ISSET(s.socket, &reads)) {
+                int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFSIZE, 0);
+                if (recvLen <= 0) {
+                    //TODO : sessions 제거
+                    //연결 끊킨 session은 벡터에서 제거
                     continue;
-
-                //Error
-                break;
-            }
-            else if (recvLen == 0) {
-                //연결 끊킴
-                break;
+                }
+                s.recvBytes = recvLen;
             }
 
-            cout << "Recv Data Len = " << recvLen << endl;
-
-            //Send
-            while (true) {
-                if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR) {
-                    if (::WSAGetLastError() == WSAEWOULDBLOCK)
-                        continue;
-
-                    //Error
-                    break;
+            //WriteCheck
+            if (FD_ISSET(s.socket, &writes)) {
+                //Send한 크기 뱉어줌.
+                //블로킹 모드 -> 모든 데이터 다 보냄
+                //논 블로킹 모드 -> 일부만 보낼 수 있음 (상대방 수신 버퍼 사황에 따라 ) <- 만약 100byte 보낼려는데, 상대방이 받을상황이
+                //안된다면 10byte만 갈 수 있다.
+                int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
+                if (sendLen == SOCKET_ERROR) {
+                    //TODO. Session 제거 
+                    continue;
                 }
 
-                cout << "Send Data ! Len = " << recvLen << endl;
-                break;
+                s.sendBytes += sendLen;
+                if (s.recvBytes == s.sendBytes) {
+                    //0, 0 초기화
+                    s.recvBytes = 0;
+                    s.sendBytes = 0;
+                }
             }
         }
 
     }
 
+    //원속 종료
     ::WSACleanup();
     return 0;
 }
