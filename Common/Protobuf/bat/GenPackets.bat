5b60a4cc8b90109dc6e1f1c3881a344788ababde
jimin Kim <programmer0415@gmail.com>
2022-12-19 10:11:37 +0900

    Chore: ProtoBuf batch 파일 추가

---
+++ Common/Protobuf/bat/GenPackets.bat
@@ -0,0 +1,19 @@
+pushd %~dp0
+protoc.exe -I=./ --cpp_out=./ ./Enum.proto
+protoc.exe -I=./ --cpp_out=./ ./Struct.proto
+protoc.exe -I=./ --cpp_out=./ ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+XCOPY /Y Enum.pb.h "../../../GameServer"
+XCOPY /Y Enum.pb.cc "../../../GameServer"
+XCOPY /Y Struct.pb.h "../../../GameServer"
+XCOPY /Y Struct.pb.cc "../../../GameServer"
+XCOPY /Y Protocol.pb.h "../../../GameServer"
+XCOPY /Y Protocol.pb.cc "../../../GameServer"
+
+XCOPY /Y Enum.pb.h "../../../DummyClient"
+XCOPY /Y Enum.pb.cc "../../../DummyClient"
+XCOPY /Y Struct.pb.h "../../../DummyClient"
+XCOPY /Y Struct.pb.cc "../../../DummyClient"
+XCOPY /Y Protocol.pb.h "../../../DummyClient"
+XCOPY /Y Protocol.pb.cc "../../../DummyClient"
